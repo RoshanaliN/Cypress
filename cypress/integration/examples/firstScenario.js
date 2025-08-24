a86b809124cee8419c89bbe8b4c5de0bbe786bd1
RoshanaliN <ec-roshanali.narsindani@ninjavan.co>
2025-08-24 20:25:22 +0530

    Cypress Practice Scenario one

---
+++ cypress/integration/examples/firstScenario.js
@@ -0,0 +1,6 @@
+//Test case is called Spec in cypress
+describe("First Suite",function(){
+    it("First Scenario",function(){
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    })
+})
