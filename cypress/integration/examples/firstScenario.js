59e84afe07152cbf4a0fd6aff88a8f9469ae8345
RoshanaliN <ec-roshanali.narsindani@ninjavan.co>
2025-08-29 23:54:00 +0530

    deep diving into cypress commands

--- cypress/integration/examples/firstScenario.js
+++ cypress/integration/examples/firstScenario.js
@@ -1,6 +1,25 @@
+/// <reference types="cypress" />
 //Test case is called Spec in cypress
 describe("First Suite",function(){
     it("First Scenario",function(){
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type("ca")
+        cy.get('.search-button')
+        cy.wait(2000)
+        // cypress get acts as findelements of selenium
+        cy.get('.product').should('have.length',5)
+        cy.get('.product:visible').should('have.length',4)
+        cy.get('.products .product').should('have.length',4)
+        // Parent Child relation-ship
+        cy.get('.products').find('.product').should('have.length',4)
+        // hardcoded
+        cy.get('.products .product').eq(1).contains('ADD TO CART').click()
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            const vegText=$el.find('h4.product-name').text()
+            if(vegText.includes('Cashews')){
+               cy.wrap($el).find('button').click() 
+            }
+
+        })
     })
 })
---
+++ readme.md
@@ -0,0 +1,6 @@
+Notes:
+Cypress only supports CSS selectors
+for id - #id -> tagname#id
+for className - .className -> tagname.classname
+for attribute - tagname[attribute='value']
+for traversing - parent child
