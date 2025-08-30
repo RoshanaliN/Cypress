8847469f327cf786a6bcd48cb306b0553bd81fb4
RoshanaliN <ec-roshanali.narsindani@ninjavan.co>
2025-08-31 00:15:53 +0530

    childWindowTabs Handling

---
+++ cypress/integration/examples/childWindow.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+describe("childWindow",function(){
+    it("childTabTest",function(){
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('#opentab').invoke('removeAttr','target').click()
+        cy.origin('https://www.qaclickacademy.com',() => {
+        cy.get('#navbarSupportedContent a[href*="about"]').click()
+        cy.get('.col-lg-5 h2').should('contain','Welcome to QAClick Academy')
+        })
+    }),
+    it("childWindowTest",function(){
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('.blinkingText').invoke('removeAttr','target').click()
+        cy.origin('https://qasummit.org',() => {
+        cy.get('#hero_section p.hero_heading').should('contain','Welcome to the Career-Focused Software Testing Meetup')
+        })
+    })
+})
